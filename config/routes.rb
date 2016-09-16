975ab8aedfefc9ebd09bc0c3791889c2d2ab8999
ivanseven <sir.ivanyeong@gmail.com>
2016-09-17 03:02:02 +0800

    FaceBook Integration

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root 'home#index'
-
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+  resources :users, only: [:show, :edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
